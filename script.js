347e11393f4cd3b1fa3887c44bb1ac32c3f626b1
Nicole-Villon-Alejandro <nicolevillon5@gmail.com>
2025-01-15 16:06:00 +0100

    add: changes

--- script.js
+++ script.js
@@ -7,12 +7,12 @@ Prima di partire a scrivere codice poniamoci qualche domanda:
 Come faccio a sapere se un numero è divisibile per?
 Abbiamo visto qualcosa di particolare che possiamo usare?*/
 
-for (let i = 1; i <= 100; i++) {
-    if (i % 3 === 0 && i % 5 === 0) {
+for ( let i = 1; i <= 100; i++ ) {
+    if ( i % 3 === 0 && i % 5 === 0) {
         console.log("FizzBuzz"); // Multipli sia di 3 che di 5
-    } else if (i % 3 === 0) {
+    } else if ( i % 3 === 0) {
         console.log("Fizz"); // Multipli di 3
-    } else if (i % 5 === 0) {
+    } else if ( i % 5 === 0) {
         console.log("Buzz"); // Multipli di 5
     } else {
         console.log(i); // Altri numeri
